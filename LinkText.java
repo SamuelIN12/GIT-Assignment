d9e8d72bce83d713187590a8e7e1cfc6e038f4ac
SamuelIN12 <32582282+SamuelIN12@users.noreply.github.com>
2017-10-20 17:29:41 -0600

    commit 1
    Initial commit for phase 1

---
+++ LinkText.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import com.*;
+import ca.*;
+/**
+ *
+ * @author iregbu2s
+ */
+public class LinkText {
+
+   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
