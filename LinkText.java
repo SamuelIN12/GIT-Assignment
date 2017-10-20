fcca7c4117ea0143891457cd4568fd1e3a463bc8
SamuelIN12 <32582282+SamuelIN12@users.noreply.github.com>
2017-10-20 17:40:15 -0600

    commit 2

--- LinkText.java
+++ LinkText.java
@@ -13,6 +13,7 @@ public class LinkText {
 
    public static void main(String[] args) {
 		// TODO Auto-generated method stub
+       
 		
 	}
 
--- com/ListElement.java
+++ com/ListElement.java
@@ -10,5 +10,41 @@ package com;
  * @author iregbu2s
  */
 public class ListElement {
+    
+    private ListElement next ;
+    private ListElement previous ;
+    private int data ;
+    
+    public ListElement ()
+    {
+        this . data = 0;
+        this . next = null ;
+        this . previous = null ;
+    }
+    
+    public ListElement (ListElement nextValue, int data1)
+    {
+        this . data = data1;
+        this . next = nextValue ;
+    }
+    
+    public void setData ( int data)
+    {
+        this . data = data ;
+    }
+    public int getData ()
+    {
+        return this . data ;
+    }
+    public ListElement getNext()
+    {
+        return next;
+    }
+    public void setNext(ListElement nextVal)
+    {
+        next = nextVal;
+    }
+    
 
-  }
+
+}
